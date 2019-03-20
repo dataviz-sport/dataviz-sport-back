9a09822a327086895096ff68d1cf756d6a96e8cf
Maxime AUBLET <maximeaublet@gmail.com>
2019-03-20 09:43:09 +0100

    first async call api ozeo

--- server.js
+++ server.js
@@ -17,7 +17,45 @@ app.listen(port, () => {
   console.log("Listening on http://localhost:" + port);
 });
 
-// GET /
+// ----------------------
+// ---- API EXPOSURE ----
+//-----------------------
+// GET ./
 app.get("/", function(req, res) {
-  res.send("hello world");
+  console.log("200: /");
+  res.send("<a href='/request'>request</a>");
 });
+
+// GET ./request
+app.get("/request", function(req, res) {
+  console.log("[IN] 200: /request");
+  articles().then(result => {
+    res.send(result);
+  });
+});
+
+// ----------------------
+// ---- HTTP Request ----
+//-----------------------
+// Domaine
+let DOMAIN_URL = "https://api.ozae.com";
+
+// Récupération des articles
+articles = async () => {
+  var url_to = `${DOMAIN_URL}/gnw/articles?query=trump&key=6c87c95a1fcc4d76bfa6d77cb5cb4d77&date=20180601__20180630`;
+
+  try {
+    let res = await axios({
+      url: url_to,
+      method: "get",
+      timeout: 10000,
+      headers: {
+        "Content-Type": "application/json"
+      }
+    });
+    if (res.status == 200) console.log("[OUT] Http Request: " + res.status);
+    return res.data;
+  } catch (error) {
+    console.error("[OUT] Http Request: " + res.status + " - " + error);
+  }
+};
